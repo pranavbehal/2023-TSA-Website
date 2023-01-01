37e7367197433ae8eb6b6b268fc40ba678b2cb07
Pranav Behal <99450292+pranavbehal@users.noreply.github.com>
2023-01-01 11:15:19 -0500

    Memory Game

--- games.js
+++ games.js
@@ -1,70 +1,92 @@
-const cards = document.querySelectorAll(".memory-card");
+document.addEventListener("DOMContentLoaded", () => {
+  const cardArray = [
+    {
+      name: "emma",
+      img: "images/emma.png",
+    },
+    {
+      name: "lily",
+      img: "images/lily.png",
+    },
+    {
+      name: "max",
+      img: "images/max.png",
+    },
+    {
+      name: "tyler",
+      img: "images/tyler.png",
+    },
+    {
+      name: "emma",
+      img: "images/emma.png",
+    },
+    {
+      name: "lily",
+      img: "images/lily.png",
+    },
+    {
+      name: "max",
+      img: "images/max.png",
+    },
+    {
+      name: "tyler",
+      img: "images/tyler.png",
+    },
+  ];
 
-let hasFlippedCard = false;
-let lockBoard = false;
-let firstCard, secondCard;
+  cardArray.sort(() => 0.5 - Math.random());
 
-function flipCard() {
-  if (lockBoard) return;
-  if (this === firstCard) return;
-  this.classList.add("flip");
+  const grid = document.querySelector(".grid");
+  const resultDisplay = document.querySelector("#result");
+  let cardsChosen = [];
+  let cardsChosenId = [];
+  let cardsWon = [];
 
-  if (!hasFlippedCard) {
-    // first click
-    hasFlippedCard = true;
-    firstCard = this;
-    return;
+  function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+      const card = document.createElement("img");
+      card.setAttribute("src", "images/blank.png");
+      card.setAttribute("data-id", i);
+      card.addEventListener("click", flipCard);
+      grid.appendChild(card);
+    }
   }
 
-  // second click
-  secondCard = this;
+  function checkForMatch() {
+    const cards = document.querySelectorAll("img");
+    const optionOneId = cardsChosenId[0];
+    const optionTwoId = cardsChosenId[1];
 
-  checkForMatch();
-}
+    if (optionOneId == optionTwoId) {
+      cards[optionOneId].setAttribute("src", "images/blank.png");
+      cards[optionTwoId].setAttribute("src", "images/blank.png");
+    } else if (cardsChosen[0] === cardsChosen[1]) {
+      cards[optionOneId].setAttribute("src", "images/transparent.png");
+      cards[optionTwoId].setAttribute("src", "images/transparent.png");
+      cards[optionOneId].removeEventListener("click", flipCard);
+      cards[optionTwoId].removeEventListener("click", flipCard);
+      cardsWon.push(cardsChosen);
+    } else {
+      cards[optionOneId].setAttribute("src", "images/blank.png");
+      cards[optionTwoId].setAttribute("src", "images/blank.png");
+    }
+    cardsChosen = [];
+    cardsChosenId = [];
+    resultDisplay.textContent = `Score: ${cardsWon.length}`;
+    if (cardsWon.length === cardArray.length / 2) {
+      resultDisplay.textContent = "Congratulations! You found them all!";
+    }
+  }
 
-function checkForMatch() {
-  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+  function flipCard() {
+    let cardId = this.getAttribute("data-id");
+    cardsChosen.push(cardArray[cardId].name);
+    cardsChosenId.push(cardId);
+    this.setAttribute("src", cardArray[cardId].img);
+    if (cardsChosen.length === 2) {
+      setTimeout(checkForMatch, 500);
+    }
+  }
 
-  isMatch ? disableCards() : unflipCards();
-}
-
-function disableCards() {
-  firstCard.removeEventListener("click", flipCard);
-  secondCard.removeEventListener("click", flipCard);
-
-  resetBoard();
-}
-
-function unflipCards() {
-  lockBoard = true;
-
-  setTimeout(() => {
-    firstCard.classList.remove("flip");
-    secondCard.classList.remove("flip");
-
-    resetBoard();
-  }, 1500);
-}
-
-function resetBoard() {
-  [hasFlippedCard, lockBoard] = [false, false];
-  [firstCard, secondCard] = [null, null];
-}
-
-(function shuffle() {
-  cards.forEach((card) => {
-    let randomPos = Math.floor(Math.random() * 12);
-    card.style.order = randomPos;
-  });
-})();
-
-cards.forEach((card) => card.addEventListener("click", flipCard));
-
-// if (document.getElementById("memory-explanation").style.display != "none") {
-//   lockBoard = true;
-// }
-
-function hideMemoryExplanation() {
-  document.getElementById("memory-explanation").style.display = "none";
-  lockBoard = false;
-}
+  createBoard();
+});

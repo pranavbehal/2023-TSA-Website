39a8ae326a531ff73b05ae7feec71694a97ee23c
Alex Sun <99508220+TourmalineDragons@users.noreply.github.com>
2023-01-03 23:27:27 +0000

    Home page

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-window.addEventListener('scroll', function () {
-    let nav = document.querySelector('nav');
-    let windowPosition = window.scrollY > 0;
-    nav.classList.toggle('scrolling-active', windowPosition);
-})
+
